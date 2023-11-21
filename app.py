32b802aaa6f4b22dfe6bc74d7bb1840961fa2fdc
jaibtdou <iamnoob2210@gmail.com>
2023-11-21 15:11:06 +0700

    Updated Ports

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import fastapi, traceback
-from fastapi import BackgroundTasks, FastAPI, Request, Response, HTTPException, Query
+from fastapi import BackgroundTasks, FastAPI, Request, HTTPException, Query
+from fastapi.responses import Response, JSONResponse
 from config import *
 import sqlite3, httpx
-import validators
+import validators, json
 app = FastAPI()
 
 client = httpx.AsyncClient()
@@ -26,81 +27,117 @@ async def index(request: Request):
     hub_verify_token = query_params.get("hub.verify_token")
     return verify_token(hub_mode, hub_challenge, hub_verify_token)
 
-# Get POST request from Facebook Messenger API
-@app.post("/")
-async def index(request: Request):
-    # Get JSON content of a request
-    # Handling error:
+@app.get("/port/{port}/{path:path}")
+@app.post("/port/{port}/{path:path}")
+@app.patch("/port/{port}/{path:path}")
+@app.put("/port/{port}/{path:path}")
+@app.delete("/port/{port}/{path:path}")
+@app.options("/port/{port}/{path:path}")
+async def dynamic_url(request: Request, port: int, path:str):
+    # Sending request
     try:
-        output = await request.json()
-        print(output)
-        page_id = ""
-        for event in output['entry']:
-            # Please read documentation of Facebook Messenger API to understand this part
-            if event.get('messaging'):
-                messaging = event['messaging']
-                for message in messaging:
-                    if message.get('message'):
-                        page_id = message['recipient']['id']
-                    elif message.get('postback'):
-                        page_id = message['recipient']['id']
-                    else:
-                        pass
+        body = await request.body()        
+        headers = dict(request.headers)
+        # Get url from database     
+        url = cursor.execute("SELECT url FROM callback WHERE port = ?", (port,)).fetchone()
+        if not url:
+            return {
+                "status": "error",
+                "message": "Port is not registered"
+            }
+        url = url[0]
+        query_params = request.query_params
+        access_token = query_params.get("access_token")
+        # Check if access_token is correct
+  
+        ACCESS_TOKEN = cursor.execute("SELECT access_token FROM callback WHERE port = ?", (port,)).fetchone()[0]
+        if not access_token and ACCESS_TOKEN:
+            return {
+                "status": "error",
+                "message": "Parameter field access_token is required for this port"
+            }
+        if access_token != ACCESS_TOKEN:
+            return {
+                "status": "error",
+                "message": "Access token is different from the one registered"
+            }
+        # Send request
+        if path: url = url + '/' + path
+        if query_params:
+            url += "?"
+            for key in query_params:
+                url += f"{key}={query_params[key]}&"
+            url = url[:-1]
+        response = await client.request(request.method, url, headers=headers, data=body)
+        print(url, body, response.headers)
+        # Return response
+        if isinstance(response.content, bytes):
+            Content = response.content.decode('utf-8')
         try:
-            url = cursor.execute("SELECT url FROM callback WHERE page_id = ?", (page_id,)).fetchone()[0]
-            response = await client.post(url, json=output)
-            print(response.content)
-            return Response(status_code=200, content="EVENT_RECEIVED")
-        except Exception as e:
-            with open('errlog.txt', 'a') as f:
-                f.write(str(e) + '\n')
-                f.write(str(traceback.format_exc()) + '\n\n\n')
+            Content = json.loads(Content)
+        except:
+            pass
+        # Return the data
+        return Content
     # If there is an error, log it to errlog.txt with trace
     except Exception as e:
         with open('errlog.txt', 'a') as f:
             f.write(str(e) + '\n')
             f.write(str(traceback.format_exc()) + '\n\n\n')
         # Return error with status 400
-        return Response(status_code=200, content="Invalid JSON")
-    # Return status 200
-    return Response(status_code=200, content="EVENT_RECEIVED")
+        return Response(status_code=400, content=str(e))
+
+# @app.get("/port/{port}")
+# @app.post("/port/{port}")
+# @app.patch("/port/{port}")
+# @app.put("/port/{port}")
+# @app.delete("/port/{port}")
+# @app.options("/port/{port}")
+# async def static_url(request: Request, port: int):
+#     return await dynamic_url(request, port, "")
 
 # Return url of a page_id
 @app.get("/get")
 async def get(request: Request):
     # Get query params
     query_params = request.query_params
-    page_id = query_params.get("page_id")
+    port = query_params.get("port")
     # Handling error:
     try:
-        url = cursor.execute("SELECT url FROM callback WHERE page_id = ?", (page_id,)).fetchone()[0]
-        return Response(status_code=200, content=url)
+        data = cursor.execute("SELECT url, description FROM callback WHERE port = ?", (port,)).fetchone()
+        data = {
+            "url": data[0],
+            "description": data[1]
+        }
+        return JSONResponse(status_code=200, content=data)
     # If there is an error, log it to errlog.txt with trace
     except Exception as e:
         with open('errlog.txt', 'a') as f:
             f.write(str(e) + '\n')
             f.write(str(traceback.format_exc()) + '\n\n\n')
         # Return error with status 400
-        return Response(status_code=400, content="Invalid page_id")
+        return Response(status_code=400, content="There was an error while retrieving the url.")
 
 @app.post("/update")
 async def update(request: Request):
     # Get the params of the request
     try:
         query_params = request.query_params
-        page_id = query_params.get("page_id")
+        port = query_params.get("port")
+        access_token = query_params.get("access_token")
+        description = query_params.get("description")
         url = query_params.get("url")
         # Remove beginning and ending spaces
-        url = url.strip()
+        if url: url = url.strip()
         # Check if url is valid or not
         if not validators.url(url):
             raise Exception("A valid URL must be provided.")
-        if not page_id or not url:
+        if not port or not url:
             raise Exception("Missing page_id or url")
-        if not cursor.execute("SELECT * FROM callback WHERE page_id = ?", (page_id,)).fetchone():
-            cursor.execute("INSERT INTO callback VALUES (?, ?)", (page_id, url))
+        if not cursor.execute("SELECT * FROM callback WHERE port = ?", (port,)).fetchone():
+            cursor.execute("INSERT INTO callback VALUES (?, ?, ?, ?)", (port, url, access_token, description))
         else:
-            cursor.execute("UPDATE callback SET url = ? WHERE page_id = ?", (url, page_id))
+            cursor.execute("UPDATE callback SET url = ?, access_token = ?, description = ? WHERE port = ?", (url, access_token, description, port))
         db.commit()
         return Response(status_code=200, content="Updated")
     # If there is an error, log it to errlog.txt with trace
@@ -110,3 +147,7 @@ async def update(request: Request):
             f.write(str(traceback.format_exc()) + '\n\n\n')
         # Return error with status 400
         return Response(status_code=400, content=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8081, reload=True)
