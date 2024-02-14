61972ca287bcf46aafe946725b558b07c4485ec5
jaibtdou <iamnoob2210@gmail.com>
2024-02-15 01:18:39 +0700

    Added ping function

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ client = httpx.AsyncClient()
 db = sqlite3.connect('database.db', check_same_thread=False)
 cursor = db.cursor()
 
+
 # Verify facebook messenger subcription
 def verify_token(hub_mode, hub_challenge, hub_verify_token):
     if hub_verify_token == META_VERIFY_TOKEN and hub_mode == 'subscribe':
         return int(hub_challenge)
     # Return Error 404 not found
     raise HTTPException(status_code=404, detail="Not found")
+
+# Setup a ping response
+@app.get("/ping")
+async def ping(request: Request):
+    query_params = request.query_params
+    url = query_params.get("url")
+    await client.get(url)
 
 # Setup a client to send requests
 @app.get("/")
